aec3cd1fe89f88c22a850eeb0c10cbb90f4950c2
capta1nLz <114829939+capta1nLz@users.noreply.github.com>
2022-10-01 16:34:47 -0700

    Add files via upload

---
+++ wasd_movement_2d/Assets/Movement.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public KeyCode jumpKey;
+
+    public float speed = 10f;
+
+    void Update()
+    {
+        Vector3 pos = transform.position;
+
+        // "w" can be replaced with any key
+        // this section moves the character up
+        if (Input.GetKey("w"))
+        {
+            pos.y += speed * 2 * Time.deltaTime;
+        }
+
+
+        // "s" can be replaced with any key
+        // this section moves the character down
+        if (Input.GetKey("s"))
+        {
+            pos.y -= speed * 2 * Time.deltaTime;
+        }
+
+        // "d" can be replaced with any key
+        // this section moves the character right
+        if (Input.GetKey("d"))
+        {
+            pos.x += speed * 2 * Time.deltaTime;
+        }
+
+        // "a" can be replaced with any key
+        // this section moves the character left
+        if (Input.GetKey("a"))
+        {
+            pos.x -= speed * 2 * Time.deltaTime;
+        }
+
+
+        transform.position = pos;
+    }
+}
